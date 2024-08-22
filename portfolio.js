0d2586e101b289856968286bf2d034c9495c4ee1
suhani yadav <2022bcasuhani11625@poornima.edu.in>
2024-08-22 10:54:05 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+let git=document.querySelector('.git');
+let insta=document.querySelector('.insta');
+git.addEventListener("click",()=>{
+window.location.href='https://github.com/Suhaniyadav07'
+})
+
+insta.addEventListener("click",()=>{
+    window.location.href='https://www.instagram.com/suhaniyadav_07/'
+    })
+    
